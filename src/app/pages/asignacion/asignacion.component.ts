9659d9e899bf75b0be2816ea8dbc962b8138a107
cadicv2 <ezio1622@gmail.com>
2025-02-11 13:33:05 -0500

    update bot

--- src/app/pages/asignacion/asignacion.component.ts
+++ src/app/pages/asignacion/asignacion.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { TuiButton } from '@taiga-ui/core'; // Importa el botón correctamente
 
 @Component({
   selector: 'app-asignacion',
-  standalone: true,
-  imports: [TuiButton], // Importa el componente en lugar del módulo
+  imports: [],
   templateUrl: './asignacion.component.html',
-  styleUrls: ['./asignacion.component.css']
+  styleUrl: './asignacion.component.css'
 })
 export class AsignacionComponent {
-  mostrarAlerta() {
-    alert('¡Hola desde Taiga UI!');
-  }
+
 }
